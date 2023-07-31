ed6b63e6e731a4ff798614c1437e527893a842e1
sathish19961412 <sathishfreelanc5@gmail.com>
2023-07-31 16:55:06 +0530

    Create Add to the Dummy Text To Components Header,Projects,Skills,TopContainer in Parent Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import Header  from './components/Header/Header';
+import TopContainer from './components/TopContainer/TopContainer';
+import Skills from './components/Skills/Skills';
+import Projects from './components/Projects/Project';
+
 const App=()=>{
   return(
     <div>
        <Header />
+       <TopContainer/>
+       <Skills />
+       <Projects/>
     </div>
   )
 }
